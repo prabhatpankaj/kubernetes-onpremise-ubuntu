d46dcfcfcbc66789aaa9cef446476c4951f0f0a7
prabhat pankaj <prabhatiitbhu@gmail.com>
2018-04-08 07:17:42 +0530

    Create configure.sh

---
+++ configure.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+sudo apt-get update \
+  && sudo apt-get install -qy docker.io
+  
+sudo apt-get update \
+  && sudo apt-get install -y apt-transport-https \
+  && curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" \
+  | sudo tee -a /etc/apt/sources.list.d/kubernetes.list \
+  && sudo apt-get update 
+  
+  sudo apt-get update \
+  && sudo apt-get install -y \
+  kubelet \
+  kubeadm \
+  kubernetes-cni
